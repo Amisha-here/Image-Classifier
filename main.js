44b012cf469d1951666c19e314f9283638330066
Amisha Purswani <ameeshapurswani@gmail.com>
2020-12-11 22:56:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,48 @@
-const image= document.getElementById('iamge');
-const dropContainer=document.getElementById('container');
+const image = document.getElementById('image'); // The image we want to classify
+const dropContainer = document.getElementById('container');
 const warning = document.getElementById('warning');
-const fileInput=document.getElementById('fileUploader');
+const fileInput = document.getElementById('fileUploader');
 
-function preventDefaults(e){
-    e.preventDefault();
-    e.stopProgagation();
+function preventDefaults(e) {
+  e.preventDefault()
+  e.stopPropagation()
 };
 
-['dragenter','dragover'].forEach(eventName => {
-    dropContainer.addEventListener(eventName,e=>dropContainer.classList.add('highlight'),false)
+['dragenter', 'dragover'].forEach(eventName => {
+  dropContainer.addEventListener(eventName, e => dropContainer.classList.add('highlight'), false)
 });
 
-['dragleave','drop'].forEach(eventName => {
-    dropContainer.addEventListener(eventName,e=>dropContainer.classList.remove('highlight'),false)
+['dragleave', 'drop'].forEach(eventName => {
+  dropContainer.addEventListener(eventName, e => dropContainer.classList.remove('highlight'), false)
 });
 
 ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-    dropContainer.addEventListener(eventName, preventDefaults, false)
-  });
+  dropContainer.addEventListener(eventName, preventDefaults, false)
+});
 
-dropContainer.addEventListener('drop',gotImage,false);
+dropContainer.addEventListener('drop', gotImage, false)
 
-function gotImage(e){
-    const dt=e.dataTransfer;
-    const files=dt.files;
-    if(files.length>1){
-        console.error('upload only one file');
+function gotImage(e) {
+  const dt = e.dataTransfer;
+  const files = dt.files;
+  if (files.length > 1) {
+    console.error('upload only one file');
+  }
+  const file = files[0];
+  const imageType = /image.*/;
+  if (file.type.match(imageType)) {
+    warning.innerHTML = '';
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      image.src = reader.result;
+      setTimeout(classifyImage, 100);
     }
-    const file=files[0];
-    const imageType=/image.*/;
-
-    if(file.type.match(imageType)){
-        warning.innerHTML='';
-        const reader= new FileReader();
-        reader.readAsDataURL(file);
-        reader.onloadend=()=>{
-            image.src=reader.result;
-            setTimeout(classifyImage,100);
-        }
-    }else{
-        image.src='images/bird.jpg';
-        setTimeout(classifyImage,100);
-        warning.innerHTML='Please drop an image file.';
-    }
+  } else {
+    image.src = 'images/bird.jpg';
+    setTimeout(classifyImage, 100);
+    warning.innerHTML = 'Please drop an image file.'
+  }
 }
 
 function handleFiles() {
@@ -59,24 +58,25 @@ function handleFiles() {
   }
 }
 
-function clickUploader(){
-    fileInput.click();
+function clickUploader() {
+  fileInput.click();
 }
 
-const result=document.getElementById('result');
-const probability=document.getElementById('probability');
-const classifier=ml5.imageClassifier('Mobilenet',()=> {});
+const result = document.getElementById('result'); 
+const probability = document.getElementById('probability'); 
+
+const classifier = ml5.imageClassifier('Mobilenet', () => {});
 
 // Make a prediction with the selected image
 // This will return an array with a default of 10 options with their probabilities
-
 classifyImage();
 
-function classifyImage(){
-    classifier.predict(image,(err,results)=>{
-        let resultTxt=results[0].className;
-        result.innerHTML=resultTxt;
-        let prob= 100* results[0].probability;
-        probability.innerText=Number.parseFloat(prob).toFixed(2) + '%';
-    });
+function classifyImage() {
+  classifier.predict(image, (err, results) => {
+    let resultTxt = results[0].className;
+    result.innerText = resultTxt;
+    let prob = 100 * results[0].probability;
+    probability.innerText = Number.parseFloat(prob).toFixed(2) + '%';
+  });
 }
+
